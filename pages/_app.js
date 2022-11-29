7f0c036b7580e5c3eb53f4b2af884dfbba7080a2
silvestre <filipesilvestre@devenz.pt>
2022-11-29 20:51:54 +0000

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
+import { useState, createContext } from "react";
+import AppContext from "../components/AppContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  const [company, setCompany] = useState("");
+  const [capvalue, setCapValue] = useState("");
+  const [phone, setPhone] = useState("");
+
+  return (
+      <AppContext.Provider value={ {company, setCompany, capvalue, setCapValue, phone, setPhone} }>
+        <Component {...pageProps} />
+      </AppContext.Provider>
+    )
+  ;
 }
 
 export default MyApp;
